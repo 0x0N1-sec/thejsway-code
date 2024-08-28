0c3897d0a5e2e4592f19c23a24b2019152810c05
0x0n1-sec <childresszac@gmail.com>
2024-08-27 19:31:45 -0500

    Finishing JS way ch 8

--- chapter08/js/word.js
+++ chapter08/js/word.js
@@ -1,3 +1,39 @@
 /*
 Word analysis
 */
+let word = prompt("Give us a word yo!");
+let vowels = 0;
+let palindrome = false;
+
+console.log(`Length: ${word.length}`);
+console.log(`${word} all lowered ${word.toLowerCase()}`);
+console.log(`${word} all uppered ${word.toUpperCase()}`);
+
+for(let i = 0; i < word.length; i++) {
+    if (word[i] === 'a' ||
+        word[i] === 'e' || 
+        word[i] === 'o' ||
+        word[i] === 'u') {
+            vowels++;
+        }
+}
+
+console.log(`Vowels within word ${vowels}`);
+
+let wordArr = Array.from(word);
+
+
+let revWordArr = wordArr.reverse();
+let revWord = "";
+
+for(let i = 0; i < word.length; i++) {
+    revWord += revWordArr[i];
+}
+
+console.log(`${word} backwards is ${revWord}`);
+
+if (word === revWord) {
+    console.log(`${word} is a palindrome of ${revWord}`);
+} else {
+    console.log(`${word} is not a palindrome`);
+}
